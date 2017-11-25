ab7fa3b7f893a2e6d92c382a629b654c2d1f99da
deepjigupta <33831104+deepjigupta@users.noreply.github.com>
2017-11-26 10:35:29 +1100

    Initial Commit

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main (String[] args)
+	{
+			System.out.println("my first java project");
+			printData D = new printData();
+			D.display();
+	}
+}
+
+	
---
+++ HelloWorld/src/printData.java
@@ -0,0 +1,7 @@
+
+public class printData {
+	public void display()
+	{
+	System.out.println("I am in class printData");
+	}
+}
